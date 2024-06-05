978e431b2f9efe718b8816e7baf0896d01429b78
MariahBitencourt <mariahhelena.bitencourt@gmail.com>
2024-06-05 17:23:06 -0300

    Csharp em IF e Else (se ou senão)

---
+++ Csharp IF e Else.cs
@@ -0,0 +1,16 @@
+﻿int idade;
+Console.WriteLine("Digite sua idade: ");
+idade = int.Parse(Console.ReadLine());
+
+if (idade >= 18)
+{
+    Console.WriteLine("Você tem " + idade + " anos");
+    Console.WriteLine("Você pode tirar carta de motorista");
+
+}
+else if (idade < 18)
+{
+    Console.WriteLine("Você tem " + idade + " anos");
+    Console.WriteLine("Você não tem idade suficiente para tirar carta de motorista");
+}
+
