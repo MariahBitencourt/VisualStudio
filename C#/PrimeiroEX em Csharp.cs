a7b53f7a578c1a3a60444814b6e76baa6c5a7292
MariahBitencourt <mariahhelena.bitencourt@gmail.com>
2024-07-31 14:37:39 -0300

    Organizando as pastas de C#

---
+++ C#/PrimeiroEX em Csharp.cs
@@ -0,0 +1,20 @@
+﻿int n1,n2,soma,subtracao,divisao,multiplicacao;
+
+Console.WriteLine("Digite o primeiro número: ");
+n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número:");
+n2 = int.Parse(Console.ReadLine());
+
+soma = n1 + n2;
+subtracao = n1 - n2;
+divisao = n1 / n2;
+multiplicacao = n1 * n2;
+
+Console.WriteLine("O resultado da soma dos números é:"+soma);
+Console.WriteLine("O resultado da subtração dos números é: " + subtracao);
+Console.WriteLine("O resultado da divisão dos números é: " + divisao);
+Console.WriteLine("O resultado da multiplicação dos números é: " + multiplicacao);
+
+
+
---
+++ C#/enquanto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+int tab,i;
+
+    Console.WriteLine("Digite um número para a tabuada:  ");
+tab = int.Parse(Console.ReadLine());
+
+
+for ( i = 0; i <= 10; i++) 
+{
+    Console.WriteLine(tab + "X" + i + "=" + (tab * i));
+}
+
+    
+    
